f79207cdccac9ef6df8f8498877f8334e16baaa8
PradheepRaajS <dheepraaj2454@gmail.com>
2026-02-27 08:32:15 +0530

    Removed server and added api spring boot project

---
+++ api/src/main/java/com/magizhdentalcare/api/MagizhDentalCarePatientManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.magizhdentalcare.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagizhDentalCarePatientManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagizhDentalCarePatientManagementApiApplication.class, args);
+	}
+
+}
